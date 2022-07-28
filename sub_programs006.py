d9cd8460a5548808cf9b920c3f061a64dcea1921
DaphineKamusiime <107240847+DaphineKamusiime@users.noreply.github.com>
2022-07-28 13:08:24 +0300

    Add files via upload

---
+++ sub_programs006.py
@@ -0,0 +1,9 @@
+#mkultipling different numbers
+def multiply(a,b):
+    print(a*b)
+multiply(2,5)
+multiply(68,5)
+multiply(3,9)
+multiply(89,5)
+multiply(21,57)
+
---
+++ sub_programs007.py
@@ -0,0 +1,9 @@
+
+from tkinter.font import names
+
+
+def names_list():
+    name=input("ENTER A NAME: ")
+    names.append(name)
+    return names
+names_list()
